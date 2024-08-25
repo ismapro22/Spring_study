26735a55b1e76762e29fe83365fa60bcf7c7863a
ismapro22 <eslove1014@naver.com>
2024-08-25 23:52:51 +0900

    Service test
    서비스 개발

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원 가입
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원
+        validateDuplicateMember(member); //회원 중복 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { //이미 값이 존재하면
+                    throw new IllegalStateException("이미 존재하는 회원임");
+                });
+    }
+
+
+//전체 회원 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
