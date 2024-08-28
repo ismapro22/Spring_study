20ace3797462f607f04f858ce873e8c26d277188
ismapro22 <eslove1014@naver.com>
2024-08-29 01:31:35 +0900

    html and register
    회원기능 추가

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,18 +1,41 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 @Controller
 public class MemberController {
-
 
     private MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+//    @ResponseStatus(HttpStatus.FOUND)
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member="+member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+//        return "home";
     }
 
 
