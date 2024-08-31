da4cf38f7790f921ac630821aaacb617a5305866
ismapro22 <eslove1014@naver.com>
2024-08-31 17:35:22 +0900

    Spring Data Jpa
    이 또한 스프링 데이터 JPA님의 은혜겠지요

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -16,25 +16,31 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
 //    private final DataSource dataSource;
+//    private EntityManager entityManager;
+    private final MemberRepository memberRepository;
 
-    private EntityManager entityManager;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager entityManager){
-//        this.dataSource = dataSource;
-        this.entityManager = entityManager;
+    @Autowired//하나면 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    @Autowired//스프링데이터JPA로 인한 변경
+//    public SpringConfig(DataSource dataSource, EntityManager entityManager){
+////        this.dataSource = dataSource;
+//        this.entityManager = entityManager;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
+//        return new MemberService(memberRepository());//스프링데이터JPA로 인한 변경
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(entityManager);
-//        return new JdbcTemplateMemberRepository(dataSource);
-//        return new JdbcMemberRepository(dataSource);//DB에 넣게
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(entityManager);//스프링데이터 JPA 쓰니까 지움. 어차피 구현체 자동으로 만들어줌
+//        return new JdbcTemplateMemberRepository(dataSource);//JPA 쓰게 지움
+//        return new JdbcMemberRepository(dataSource);//DB에 넣게 지움
 //        return new MemoryMemberRepository();
-    }
+//    }
 }
